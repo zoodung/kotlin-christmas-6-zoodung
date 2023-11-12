eec1792aad9a007d09f2d2006353f34f9f4a2518
zoodung <zoodung@naver.com>
2023-11-12 11:34:59 +0900

    docs(README): 메뉴와 개수 입력 기능 수정

--- src/test/kotlin/christmas/CustomerTest.kt
+++ src/test/kotlin/christmas/CustomerTest.kt
@@ -9,6 +9,8 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class CustomerTest {
+
+
     @ValueSource(strings = ["", "Tomorrow", "12.11", " 25", "25 ", "-1", "0", "32"])
     @ParameterizedTest
     fun `예상 방문 날짜 예외 처리`(input: String) {
@@ -19,7 +21,7 @@ class CustomerTest {
 
     @ValueSource(strings = ["", "초코케이크2"])
     @ParameterizedTest
-    fun `주문 메뉴와 개수 예외 처리1`(input: String) {
+    fun `주문 메뉴와 개수 스플릿 전 예외 처리`(input: String) {
         assertThrows<IllegalArgumentException> {
             validateOrderMenuBeforeSplit(input)
         }
@@ -27,7 +29,7 @@ class CustomerTest {
 
     @ValueSource(strings = ["당근케이크-1", "초코케이크-a", "초코케이크-0", "초코케이크-5,샴페인-16", "초코케이크-5,초코케이크-5", "제로콜라-5,샴페인-1"])
     @ParameterizedTest
-    fun `주문 메뉴와 개수 예외 처리2`(input: String) {
+    fun `주문 메뉴와 개수 스플릿 후 예외 처리`(input: String) {
         assertThrows<IllegalArgumentException> {
             validateOrderMenuAfterSplit(StoreMenu.splitOrderItems(input))
         }
