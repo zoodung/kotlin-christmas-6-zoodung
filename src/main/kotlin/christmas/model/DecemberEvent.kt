fd175e444b7722c8cd753dad11902e7e7ca4b119
zoodung <zoodung@naver.com>
2023-11-13 10:11:57 +0900

    refactor(DecemberEvent): applyWeekdayDiscount(), applyWeekendDiscount() depth 감소

--- src/main/kotlin/christmas/model/DecemberEvent.kt
+++ src/main/kotlin/christmas/model/DecemberEvent.kt
@@ -19,30 +19,38 @@ class DecemberEvent {
         var dessertMenuCount = INITIALIZE_NUMBER
 
         for ((menuName, quantity) in orderMenu) {
-            if (increaseDessertMenuCount(menuName))
-                dessertMenuCount += quantity
+            val increaseCount = increaseDessertMenuCount(menuName, quantity)
+            dessertMenuCount += increaseCount
         }
 
         return dessertMenuCount * WEEK_DISCOUNT_AMOUNT
     }
 
-    private fun increaseDessertMenuCount(menuName: String): Boolean =
-        (menuName == StoreMenu.CHOCO_CAKE.menuName || menuName == StoreMenu.ICE_CREAM.menuName)
+    private fun increaseDessertMenuCount(menuName: String, quantity: Int): Int {
+        if (menuName == StoreMenu.CHOCO_CAKE.menuName || menuName == StoreMenu.ICE_CREAM.menuName)
+            return quantity
+        return INITIALIZE_NUMBER
+    }
+
 
     fun applyWeekendDiscount(orderMenu: List<Pair<String, Int>>): Int {
         var mainMenuCount = INITIALIZE_NUMBER
 
         for ((menuName, quantity) in orderMenu) {
-            if (increaseMainMenuCount(menuName))
-                mainMenuCount += quantity
+            val increaseCount = increaseMainMenuCount(menuName, quantity)
+            mainMenuCount += increaseCount
         }
 
         return mainMenuCount * WEEK_DISCOUNT_AMOUNT
     }
 
-    private fun increaseMainMenuCount(menuName: String): Boolean =
-        (menuName == StoreMenu.T_BONE_STEAK.menuName || menuName == StoreMenu.BBQ_RIBS.menuName ||
-                menuName == StoreMenu.SEAFOOD_PASTA.menuName || menuName == StoreMenu.CHRISTMAS_PASTA.menuName)
+    private fun increaseMainMenuCount(menuName: String, quantity: Int): Int {
+        if (menuName == StoreMenu.T_BONE_STEAK.menuName || menuName == StoreMenu.BBQ_RIBS.menuName ||
+            menuName == StoreMenu.SEAFOOD_PASTA.menuName || menuName == StoreMenu.CHRISTMAS_PASTA.menuName
+        )
+            return quantity
+        return INITIALIZE_NUMBER
+    }
 
     fun applySpecialDiscount(): Int = CHRISTMAS_DAY_DISCOUNT_AMOUNT
 
